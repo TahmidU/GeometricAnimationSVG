7d4b3a28729c1e88c2881c84d6bef7992ab63d21
TahmidU <tahmiduddin@hotmail.co.uk>
2019-11-13 15:54:13 +0000

    All points are sorted based on the ones closest to the center.

--- ACWF634Animation.hs
+++ ACWF634Animation.hs
@@ -1,10 +1,25 @@
 module ACWF634Animation where
 
 import Animation
+import Data.List
 
 --Screen properties
+------------------------------------------------------------------------------------
+
 width = 800
 height = 600
+
+--Math Functions
+------------------------------------------------------------------------------------
+
+pyDistance :: (Double,Double) -> (Animation, (Double, Double)) -> Double
+pyDistance (x1,y1) (a,(x2,y2)) = sqrt (cx*cx + cy*cy)
+ where
+    cx = x1 - x2
+    cy = y1 - y2
+    
+--Renders
+------------------------------------------------------------------------------------    
 
 --Black background
 background :: Animation
@@ -16,12 +31,29 @@ cCircle :: Animation
 cCircle = (withBorder (always white) (always 2) (withoutPaint (circle (always 20))))
 
 --Pattern: 20*15
-pattern :: [Animation]
-pattern = [(translate (always (40*i,40*j)) cCircle) | i <- [0..20], j <- [0..15]]
+pattern :: [(Animation, (Double,Double))]
+pattern = [((translate (always (40*i,40*j)) cCircle), (i*40,j*40)) | i <- [0..20], j <- [0..15]]
+
+--Sort Patterns closest to the center.
+sortPattern :: [(Animation, (Double,Double))] -> [(Animation, (Double,Double))]
+sortPattern xs = sortOn (pyDistance (400,300)) [(a,(x,y)) | (a,(x,y)) <- xs]
 
 --Main picture
 picture :: Animation
-picture = background `plus` (combine pattern)
+picture = background
+
+--Helper/Debugger Functions
+------------------------------------------------------------------------------------
+
+coordPrinter :: [(Animation, (Double,Double))] -> [(Double,Double)]
+coordPrinter xs = [(x,y) | (a,(x,y)) <- xs]
+
+
+mark :: Animation
+mark = (translate (always (400,300)) (withPaint (always red) (circle (always 20))))
+
+--Outputs
+------------------------------------------------------------------------------------
 
 --Test animation individually
 singleAnimation :: String -> Animation -> IO()
@@ -31,5 +63,6 @@ singleAnimation x y = writeFile x (svg width height y)
 outFile :: IO()
 outFile = writeFile "tahmid_uddin_acwf634.svg" (svg width height picture)
 
+--Compile using stack
 main :: IO()
 main = outFile
