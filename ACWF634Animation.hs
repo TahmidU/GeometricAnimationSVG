1038b969bbac922c3a12827430cec75c11502fb8
TahmidU <tahmiduddin@hotmail.co.uk>
2019-11-13 22:07:54 +0000

    Distance from point to center are now stored with its associated animation. More Helper functions added.

--- ACWF634Animation.hs
+++ ACWF634Animation.hs
@@ -11,12 +11,19 @@ height = 600
 
 --Math Functions
 ------------------------------------------------------------------------------------
-
-pyDistance :: (Double,Double) -> (Animation, (Double, Double)) -> Double
-pyDistance (x1,y1) (a,(x2,y2)) = sqrt (cx*cx + cy*cy)
+--Helper for sorting animations by calculating distance from point to center.
+sortDistanceHelper :: (Double,Double) -> (Animation, (Double, Double)) -> Double
+sortDistanceHelper (x1,y1) (a,(x2,y2)) = sqrt (cx*cx + cy*cy)
  where
     cx = x1 - x2
     cy = y1 - y2
+
+--Calculate distance from point to center.    
+pyDistance :: (Double,Double) -> (Double, Double) -> Double
+pyDistance (x1,y1) (x2,y2) = sqrt (cx*cx + cy*cy)
+ where
+    cx = x1 - x2
+    cy = y1 - y2    
     
 --Renders
 ------------------------------------------------------------------------------------    
@@ -35,12 +42,16 @@ pattern :: [(Animation, (Double,Double))]
 pattern = [((translate (always (40*i,40*j)) cCircle), (i*40,j*40)) | i <- [0..20], j <- [0..15]]
 
 --Sort Patterns closest to the center.
-sortPattern :: [(Animation, (Double,Double))] -> [(Animation, (Double,Double))]
-sortPattern xs = sortOn (pyDistance (400,300)) [(a,(x,y)) | (a,(x,y)) <- xs]
+sortPattern :: [(Animation, (Double,Double))] -> [(Animation, Double)]
+sortPattern xs = [ (a,(pyDistance (400,300) (x,y))) |  (a,(x,y)) <- (sortOn (sortDistanceHelper (400,300)) xs)]
 
 --Main picture
 picture :: Animation
 picture = background
+
+--Experimental Picture
+pic :: Animation
+pic = scale (cycleSmooth 2 [(0,0),(1,1)]) (withPaint (always green) (circle (always 20)))
 
 --Helper/Debugger Functions
 ------------------------------------------------------------------------------------
@@ -48,6 +59,8 @@ picture = background
 coordPrinter :: [(Animation, (Double,Double))] -> [(Double,Double)]
 coordPrinter xs = [(x,y) | (a,(x,y)) <- xs]
 
+distancePrinter :: [(Animation, Double)] -> [Double]
+distancePrinter xs = [d | (a,d) <- xs]
 
 mark :: Animation
 mark = (translate (always (400,300)) (withPaint (always red) (circle (always 20))))
