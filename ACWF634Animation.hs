345eb4cacb100aaf4032caa100eab229373de7bb
TahmidU <tahmiduddin@hotmail.co.uk>
2019-11-14 20:55:03 +0000

    cCircle, pattern (now patternCord), sortPattern (now SortCoord) functions editted. sortCoord and patternCord now only work with cordinations x and y rather than with animation and cordinates.

--- ACWF634Animation.hs
+++ ACWF634Animation.hs
@@ -9,22 +9,20 @@ import Data.List
 width = 800
 height = 600
 
---Math Functions
+--Helper Functions
 ------------------------------------------------------------------------------------
---Helper for sorting animations by calculating distance from point to center.
-sortDistanceHelper :: (Double,Double) -> (Animation, (Double, Double)) -> Double
-sortDistanceHelper (x1,y1) (a,(x2,y2)) = sqrt (cx*cx + cy*cy)
- where
-    cx = x1 - x2
-    cy = y1 - y2
 
---Calculate distance from point to center.    
+--Helps to sort out the list containing: (Double,Double).
+sortHelper :: (Double,Double) -> (Double, Double) -> Double
+sortHelper (x1,y1) (x2,y2) = pyDistance (x1,y1) (x2,y2)
+
+--Calculate distance from point to point.    
 pyDistance :: (Double,Double) -> (Double, Double) -> Double
 pyDistance (x1,y1) (x2,y2) = sqrt (cx*cx + cy*cy)
  where
     cx = x1 - x2
-    cy = y1 - y2    
-    
+    cy = y1 - y2
+
 --Renders
 ------------------------------------------------------------------------------------    
 
@@ -34,33 +32,38 @@ background = translate (always (0,0))
  (withPaint (always black) (rect (always 800) (always 600)))
 
 --Shape
-cCircle :: Animation
-cCircle = (withBorder (always white) (always 2) (withoutPaint (circle (always 20))))
+cCircle :: (Double,Double) -> Animation
+cCircle (x,y) = (translate (always (x,y)) 
+ (scale (repeatSmooth (1,1) [(1,(1,1)),(2,(2,2)),(3,(1,1))]) (withBorder (always white) (always 2) (withoutPaint (circle (always 20))))))
 
 --Pattern: 20*15
-pattern :: [(Animation, (Double,Double))]
-pattern = [((translate (always (40*i,40*j)) cCircle), (i*40,j*40)) | i <- [0..20], j <- [0..15]]
+patternCoord :: [(Double,Double)]
+patternCoord = [ (i*40,j*40) | i <- [0..20], j <- [0..15]]
 
 --Sort Patterns closest to the center.
-sortPattern :: [(Animation, (Double,Double))] -> [(Animation, Double)]
-sortPattern xs = [ (a,(pyDistance (400,300) (x,y))) |  (a,(x,y)) <- (sortOn (sortDistanceHelper (400,300)) xs)]
+sortCoord :: [(Double,Double)] -> [(Double, Double)]
+sortCoord xs = [ (x,y) |  (x,y) <- (sortOn (sortHelper (400,300)) xs)]
+
+animatePattern :: [(Double,Double)] -> [Animation]
+animatePattern xs = [cCircle (x,y) | (x,y) <- xs]
 
 --Main picture
 picture :: Animation
-picture = background
+picture = background `plus` (combine (animatePattern (sortCoord patternCoord)))
 
 --Experimental Picture
-pic :: Animation
-pic = scale (cycleSmooth 2 [(0,0),(1,1)]) (withPaint (always green) (circle (always 20)))
+--pic :: Animation
+--pic = background `plus` (scale (repeatSmooth (1,1) [(1,(1,1)),(2,(2,2)),(3,(1,1))]) (translate (always (360,300)) cCircle))
+ --`plus` (scale (repeatSmooth (1,1) [(1,(1,1)),(2,(2,2)),(3,(1,1))]) (translate (always (400,300)) cCircle))
 
 --Helper/Debugger Functions
 ------------------------------------------------------------------------------------
 
-coordPrinter :: [(Animation, (Double,Double))] -> [(Double,Double)]
-coordPrinter xs = [(x,y) | (a,(x,y)) <- xs]
+coordPrinter :: [(Double,Double)] -> [(Double,Double)]
+coordPrinter xs = [(x,y) | (x,y) <- xs]
 
-distancePrinter :: [(Animation, Double)] -> [Double]
-distancePrinter xs = [d | (a,d) <- xs]
+--distancePrinter :: [(Animation, Double)] -> [Double]
+--distancePrinter xs = [d | (a,d) <- xs]
 
 mark :: Animation
 mark = (translate (always (400,300)) (withPaint (always red) (circle (always 20))))
